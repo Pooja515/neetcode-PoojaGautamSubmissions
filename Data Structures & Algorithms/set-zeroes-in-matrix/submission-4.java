e12e7a702bf2cb0c66a28da5eafd7ed15659eb22
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-01 03:53:59 +0530

    Add: set-zeroes-in-matrix - submission-4

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-4.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean rowflag = false , colflag = false; 
+        int m = matrix.length , n = matrix[0].length;
+        
+        for(int i =0 ; i<m;i++){
+            if(matrix[i][0] == 0){
+                colflag = true;
+                break;
+            }
+        }
+
+        for(int i =0 ; i<n;i++){
+            if(matrix[0][i] == 0){
+                rowflag = true;
+                break;
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1 ; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] =0;
+                    matrix[0][j] =0;
+    
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1 ; j<n; j++){
+                if( matrix[i][0] ==0 || matrix[0][j] ==0){
+                    matrix[i][j] =0;
+                }
+            }
+        }
+        if(rowflag){
+            for(int i =0 ; i<n ;i++){
+                matrix[0][i] =0;
+            
+            }
+        }
+         if(colflag){
+            for(int i =0 ; i<m ;i++){
+                matrix[i][0] =0;
+            
+            }
+        }
+    }
+}
